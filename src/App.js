9d8f31bea973af3fe0690c3eb0d50151de7f289b
Emeka Okoli <emekaokoli00@gmail.com>
2022-04-03 18:40:41 +0100

    fully working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+import headers from './utils/headers';
+import TableView from './components/Table/TableView';
+
 function App() {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      const { data } = await axios(
+        'https://liquality.io/swap/agent/api/swap/marketinfo',
+        {
+          headers,
+        },
+      );
+      setResults(data);
+      setLoading(false);
+    };
+    setInterval(fetchData, 3000);
+    // clear interval
+    return () => {
+      clearInterval(fetchData);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container'>
+      {loading === true ? (
+        <div className='App'>Loading...</div>
+      ) : (
+        <TableView results={results} />
+      )}
     </div>
   );
 }
